7f3151cde26ebb4df63ed8b037b760bb92f73df4
Rui Santos <rfgsantos>
2022-10-07 08:23:12 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sme_starter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sme_starter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Título Gestor de Janelas',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(title: 'Título Appbar'),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+          child: Text(
+        'Olá Mundo!!!',
+      )),
+    );
+  }
+}
